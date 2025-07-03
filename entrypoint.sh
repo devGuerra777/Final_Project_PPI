51bdf258fe6aba98d34bc3df03bf9f55a898c534
Guerra <ramiro.amador.guerra@gmail.com>
2025-07-03 14:24:26 -0600

    docker no tenia nc instalado

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# Espera a que la base de datos MySQL esté lista
-until nc -z -v -w30 $DB_HOST $DB_PORT
-do
-  echo "Esperando a que MySQL ($DB_HOST:$DB_PORT) esté disponible..."
-  sleep 5
-done
+echo "Esperando 15 segundos para que la base de datos esté lista..."
+sleep 15
 
-# Ejecuta las migraciones de Laravel automáticamente
 php artisan migrate --force
-
-# Limpia cache de configuración (opcional pero recomendado)
 php artisan config:cache
 
-# Ejecuta Apache en primer plano para mantener el contenedor activo
 exec apache2-foreground
